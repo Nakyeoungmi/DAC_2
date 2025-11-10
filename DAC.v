b8b3fb42b268a5910954e8abcd987a2cf4453895
Nakyeoungmi <qwerty5506a@naver.com>
2025-11-10 11:18:54 +0900

    Update DAC.v

--- DAC.v
+++ DAC.v
@@ -7,12 +7,13 @@ module DAC(
     output reg dac_csn, dac_ldacn, dac_wrn, dac_a_b,
     output reg [7:0] dac_d,
     output reg [7:0] led_out,
-    output [6:0] seg,       
-    output [3:0] seg_sel,  
+
+    output [6:0] seg,
+    output [3:0] seg_sel,
+
     output LCD_E, LCD_RS, LCD_RW,
     output [7:0] LCD_DATA
 );
-
 
 reg [7:0] dac_d_temp;
 reg [7:0] cnt;
@@ -37,22 +38,22 @@ always @(posedge clk or negedge rst) begin
 end
 
 always @(posedge clk or negedge rst) begin
-    if(!rst) cnt <= 8'b0000_0000;
+    if(!rst) cnt <= 8'd0;
     else begin
         case(state)
-            DELAY   : if(cnt >= 200) cnt <= 0; else cnt <= cnt + 1;
-            SET_WRN : if(cnt >= 50)  cnt <= 0; else cnt <= cnt + 1;
-            UP_DATA : if(cnt >= 30)  cnt <= 0; else cnt <= cnt + 1;
+            DELAY   : cnt <= (cnt >= 8'd200) ? 8'd0 : cnt + 1'b1;
+            SET_WRN : cnt <= (cnt >= 8'd50 ) ? 8'd0 : cnt + 1'b1;
+            UP_DATA : cnt <= (cnt >= 8'd30 ) ? 8'd0 : cnt + 1'b1;
         endcase
     end
 end
 
 always @(posedge clk or negedge rst) begin
-    if(!rst) dac_wrn <= 1;
+    if(!rst) dac_wrn <= 1'b1;
     else begin
         case(state)
-            DELAY   : dac_wrn <= 1;
-            SET_WRN : dac_wrn <= 0;
+            DELAY   : dac_wrn <= 1'b1;
+            SET_WRN : dac_wrn <= 1'b0;
             UP_DATA : dac_d   <= dac_d_temp;
         endcase
     end
@@ -60,31 +61,37 @@ end
 
 always @(posedge clk or negedge rst) begin
     if(!rst) begin
-        dac_d_temp <= 8'b0000_0000;
-        led_out    <= 8'b0101_0101;
-    end else begin
-        if(btn_t == 6'b100000) dac_d_temp <= dac_d_temp - 8'd1;
-        else if(btn_t == 6'b010000) dac_d_temp <= dac_d_temp + 8'd1;
-        else if(btn_t == 6'b001000) dac_d_temp <= dac_d_temp - 8'd2;
-        else if(btn_t == 6'b000100) dac_d_temp <= dac_d_temp + 8'd2;
-        else if(btn_t == 6'b000010) dac_d_temp <= dac_d_temp - 8'd8;
-        else if(btn_t == 6'b000001) dac_d_temp <= dac_d_temp + 8'd8;
+        dac_d_temp <= 8'd0;
+        led_out    <= 8'h55;
+    end
+    else begin
+        if      (btn_t == 6'b100000) dac_d_temp <= dac_d_temp - 8'd1;
+        else if (btn_t == 6'b010000) dac_d_temp <= dac_d_temp + 8'd1;
+        else if (btn_t == 6'b001000) dac_d_temp <= dac_d_temp - 8'd2;
+        else if (btn_t == 6'b000100) dac_d_temp <= dac_d_temp + 8'd2;
+        else if (btn_t == 6'b000010) dac_d_temp <= dac_d_temp - 8'd8;
+        else if (btn_t == 6'b000001) dac_d_temp <= dac_d_temp + 8'd8;
 
         led_out <= dac_d_temp;
     end
 end
 
 always @(posedge clk) begin
-    dac_csn   <= 0;
-    dac_ldacn <= 0;
+    dac_csn   <= 1'b0;
+    dac_ldacn <= 1'b0;
     dac_a_b   <= add_sel; 
 end
 
 
 wire [3:0] bcd0, bcd1, bcd2;
-binary_to_bcd B2BCD(.bin(dac_d_temp), .bcd2(bcd2), .bcd1(bcd1), .bcd0(bcd0));
+binary_to_bcd u_b2b (
+    .bin (dac_d_temp),
+    .bcd2(bcd2),
+    .bcd1(bcd1),
+    .bcd0(bcd0)
+);
 
-seg7_dynamic SEGDRV(
+seg7_dynamic u_seg (
     .clk(clk),
     .rst(rst),
     .digit3(bcd2),
@@ -96,11 +103,10 @@ seg7_dynamic SEGDRV(
 );
 
 
-text_LCD_basic LCD_DRV(
-    .rst(rst),
+text_LCD_basic u_lcd (
     .clk(clk),
-    .line1("DAC Value: "),
-    .line2(dac_d_temp),
+    .rst(rst),
+    .val(dac_d_temp),
     .LCD_E(LCD_E),
     .LCD_RS(LCD_RS),
     .LCD_RW(LCD_RW),
