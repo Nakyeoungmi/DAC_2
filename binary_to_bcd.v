d1e2e5f38871ee8b0b7380c1538b79ff35b5af53
Nakyeoungmi <qwerty5506a@naver.com>
2025-11-10 11:19:13 +0900

    Update binary_to_bcd.v

--- binary_to_bcd.v
+++ binary_to_bcd.v
@@ -1,14 +1,18 @@
+`timescale 1ns / 1ps
 module binary_to_bcd(
-    input [7:0] bin,
-    output reg [3:0] bcd2, bcd1, bcd0
+    input  [7:0] bin,
+    output reg [3:0] bcd2,  
+    output reg [3:0] bcd1,  
+    output reg [3:0] bcd0   
 );
     integer i;
     reg [19:0] shift;
+
     always @(*) begin
-        shift = 0;
+        shift = 20'd0;
         shift[7:0] = bin;
         for(i=0;i<8;i=i+1) begin
-            if(shift[11:8] >= 5) shift[11:8] = shift[11:8] + 3;
+            if(shift[11:8] >= 5)  shift[11:8]  = shift[11:8]  + 3;
             if(shift[15:12] >= 5) shift[15:12] = shift[15:12] + 3;
             if(shift[19:16] >= 5) shift[19:16] = shift[19:16] + 3;
             shift = shift << 1;
